aff3a60cf65e234c824fd722faf3d03c95498a89
renukagade <128017075+renukagade@users.noreply.github.com>
2024-06-24 15:23:35 +0530

    Update Dict2.py

--- Dict2.py
+++ Dict2.py
@@ -3,7 +3,7 @@ import requests
 import speech_recognition as sr
 import pyttsx3
 from googletrans import Translator
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, clientSettings
 import av
 import time
 
